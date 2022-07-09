47d703d31f947bbd7f3eaa5f905fa74cfd3b04d5
sadam <sadamccr@gmail.com>
2022-07-09 17:24:50 +0500

    book project initial commit

---
+++ lib/book.dart
@@ -0,0 +1,17 @@
+class Book {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  bool isFavorite;
+
+  Book({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.imageUrl,
+    this.isFavorite = false,
+  });
+}
